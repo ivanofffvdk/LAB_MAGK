e9e8d20df70f4282562da269da59b34325d81ca5
ivanofffvdk <kozybka00@mail.ru>
2023-10-02 11:40:56 +1000

    second commit

--- LAB1.py
+++ LAB1.py
@@ -1,1 +1,17 @@
-print('Hello World')
+# y^2 = x^3  + 31988x + 1000
+
+A = 31988
+B = 1000
+x0 = 0
+y0 = 5585
+p = 31991
+
+
+# P = P
+def func():
+    lam = ((3 * pow(x0, 2) + A) / (2 * y0)) % p
+    x = (pow(lam, 2) - 2 * x0) % p
+    y = (y0 + lam * (x - x0)) % p
+    return [x, y]
+
+print(pow(A / (2 * 5585), 1, p))
