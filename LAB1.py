f2e28b1d8ca3fcf8aba1b7d2678f8c0699277666
ivanofffvdk <kozybka00@mail.ru>
2023-10-03 15:36:10 +1000

    4 commit

--- LAB1.py
+++ LAB1.py
@@ -1,5 +1,5 @@
 # y^2 = x^3  + 31988x + 1000
-
+import matplotlib.pyplot as plt
 A = 31988
 B = 1000
 x0 = 0
@@ -17,22 +17,29 @@ def check(p, y, x, A, B):
 
 # P = P
 def P():
-    for i in range(1, 3): # 32089
-        P1 = points[i -1]
+    for i in range(1, 32089): # 32089
+        P1 = points[i - 1]
         P2 = points[i]
         if P1 == P2:
-            lam = ((3 * pow(P1[0], 2) + A) / (2 * P1[1])) % p
-            x3 = round((pow(lam, 2) - 2 * P1[0]) % p)
-            y3 = round((y0 + lam * (x3 - P1[0])) % p)
+            lam = pow(round(((3 * pow(P1[0], 2) + A) / (2 * P1[1]))), 1, p)
+            x3 = pow(round((pow(lam, 2) - 2 * P1[0]) % p), 1, p)
+            y3 = pow(round((y0 + lam * (x3 - P1[0])) % p), 1, p)
             #if check(p, y3, x3, A, B ) == True: #по этому условию не проходит, не принадлежит прямой
             points.append([x3, y3])
+
+
         elif points[i] != points[i - 1]:
-            lam = round(((P2[1] -P1[1]) / (P2[0] - P1[0])) % p)
-            x3 = lam ** 2 - P1[0] - P2[0] % p # тут p относится ко всему выражению или нет?
+            lam = round(((P2[1] - P1[1]) / pow((P2[0] - P1[0]), 1, p)))
+            x3 = pow(lam ** 2 - P1[0] - P2[0], 1, p) # тут p относится ко всему выражению или нет?
             y3 = P1[1] + (lam * (x3 - P1[0])) % p
             # if check(p, y3, x3, A, B ) == True: #по этому условию не проходит, не принадлежит прямой
             points.append([x3, y3])
-        elif P([]): # не понял
+
+        elif P1[1] == (- P2[1]) % p: # не понял
+            lam = ((3 * pow(P1[0], 2) + A) * pow(2 * P1[1], -1, p)) % p
+            x3 = pow(lam, 2) - (2 * P1[0]) % p
+            y3 = pow(P1[1] + lam * (x3 - P1[0]),1, p)
+            points.append([x3, y3])
             ...
 
 
@@ -45,6 +52,7 @@ def func():
 P()
 print(type(points[0][0]))
 print(points)
-
+#plt.scatter(x3, y3, s=0.7)
+#plt.show()
 
 
